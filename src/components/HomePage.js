a9a88449b1895915035faca233ee1d40d62fecb4
akash_garg <akashgarg3a@gmail.com>
2022-01-30 20:48:38 +0530

    Basic setup and HomePage Component

---
+++ src/components/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+    <div className='jumbotron'>
+        <h1>PluralSight Adminstration</h1>
+        <p>Using flux</p>
+    </div>
+    );
+}
