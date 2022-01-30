7aae2a34b51b8cdacd01a9afcb6f14f2a5467e02
akash_garg <akashgarg3a@gmail.com>
2022-01-30 21:30:49 +0530

    Added Routing

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,6 +5,7 @@ function HomePage() {
     <div className='jumbotron'>
         <h1>PluralSight Adminstration</h1>
         <p>Using flux</p>
+        <a href='/about'>About Page</a>
     </div>
     );
 }
