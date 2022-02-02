c81f762d47002db0cb4168c61f337e0a46daaa2b
akash_garg <akashgarg3a@gmail.com>
2022-02-02 23:31:40 +0530

    Page Not Found Added

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,19 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import SpeakersPage from "./SpeakersPages";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
     return (
         <div className="contaner-fluid">
             <Header />
-            <Route path="/" exact component={HomePage}></Route>
-            <Route path="/About" exact component={AboutPage}></Route>
-            <Route path="/Speakers" component={SpeakersPage}></Route>
+            <Switch>
+                <Route path="/" exact component={HomePage}></Route>
+                <Route path="/About" exact component={AboutPage}></Route>
+                <Route path="/Speakers" component={SpeakersPage}></Route>
+                <Route component={NotFoundPage} />
+            </Switch>
         </div>
     );
 }
